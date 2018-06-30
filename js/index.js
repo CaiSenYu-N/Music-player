453dc43997ed1f672370878ff565ba02b1fe1586
caisenyu <392818050@qq.com>
2018-06-30 21:41:50 +0800

    add

---
+++ js/index.js
@@ -0,0 +1,65 @@
+var EventCenter = {
+    on: function(type, handler){
+        $(document).on(type, handler)
+    },
+    fire: function(type, data){
+        $(document).trigger(type, data)
+  }
+}
+// var Footer = {
+//     init: function(){
+
+//     },
+//     bind: function(){
+
+//     },
+//     render: function(){
+
+//     }
+// }
+
+// Footer.init()
+
+var Footer = {
+    init: function(){
+        this.$footer = $('footer')
+        this.bind()
+        this.render()
+    },
+
+    bind: function(){
+        var _this = this
+        $(window).resize(function(){
+            _this.setStyle()
+        })
+    },
+
+    render(){
+        var _this = this
+        $.getJSON('http://api.jirengu.com/fm/getChannels.php')
+        .done(function(ret){
+            console.log(ret)
+            _this.renderFooter(ret.channels)
+        }).fail(function(){
+            console.log('error')
+        })
+    },
+
+    renderFooter: function(channels){
+        console.log(channels)
+        var html = ''
+        channels.forEach(function(channel){
+            html += '<li data-channel-id='+channel.channel_id+' data-channel-name='+channel.name+'>'
+                    + '  <div class="cover" style="background-image:url('+channel.cover_small+')"></div>'
+                    + '  <h3>'+channel.name+'</h3>'
+                    +'</li>'
+        })
+        this.$footer.find('ul').html(html)
+        this.setStyle()
+    },
+    setStyle: function(){
+
+    }
+}
+
+Footer.init()
